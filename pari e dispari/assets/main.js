9412ea61726a61fd0c61f973aa73dffccdfccb36
Stefania9797 <stefaniamastrel@gmail.com>
2021-04-29 20:10:16 +0200

    add alert

--- pari e dispari/assets/main.js
+++ pari e dispari/assets/main.js
@@ -1,10 +1,12 @@
 //L’utente sceglie pari o dispari 
-var oddOrEven = prompt("Pari o Dispari?");
+var oddOrEven = prompt("pari o dispari?");
 //e inserisce un numero da 1 a 5.
 var userNumber = Number(prompt("Inserisci un numero da 1 a 5"));
 console.log(userNumber);
+alert("il tuo numero è "+userNumber);
 var pcNumber = RandomNumber(1,6);
 console.log(pcNumber);
+alert("il numero del computer è "+pcNumber);
 //Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 function RandomNumber(min, max) {
     min = Math.ceil(min);
@@ -18,18 +20,23 @@ console.log(somma);
 function isEven(number_1 , number_2){
     if((number_1 + number_2) % 2==0){
         console.log("la somma è pari");
+        alert("la somma dei numeri è pari");
         return true;
         
     }
     console.log("la somma è dispari");
+    alert("la somma dei numeri è dispari");
     return false;
 }
 //Dichiariamo chi ha vinto.
-if(oddOrEven =="Pari" && isEven(userNumber , pcNumber)){
+if(oddOrEven =="pari" && isEven(userNumber , pcNumber)){
     console.log("vince l'utente");
-}else if(oddOrEven =="Dispari" && !isEven(userNumber , pcNumber)){
+    alert("Hai vinto!");
+}else if(oddOrEven =="dispari" && !isEven(userNumber , pcNumber)){
     console.log("vince l'utente");
+    alert("Hai vinto!");
 }
 else{
     console.log("vince il pc");
+    alert("Hai perso!");
 }
